e8a0501fac4a71997238668f92a74d2a7a57c8e2
Oerd Bej <oerdbej@gmail.com>
2023-04-25 08:52:42 +0200

    changed structure for new project, boiler plate set ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import people from './components/data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [index, setIndex] = useState(0);
+
+    const { name, job, image, text } = people[index];
+    console.log(name);
+
+    return <div>App</div>;
+};
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Rick Rubin',
+        job: 'Music Producer',
+        image: 'https://ichef.bbci.co.uk/images/ic/640x360/p0216jdj.jpg',
+        text: 'All that matters is that you are making something you love, to the best of your ability, here and now.',
+    },
+    {
+        id: 2,
+        name: 'Joe Rogan',
+        job: 'Podcaster, Comedian',
+        image: 'https://hips.hearstapps.com/hmg-prod/images/gettyimages-174032192-1619620597.jpeg?crop=0.536xw:1.00xh;0.158xw,0&resize=1200:*',
+        text: 'There is only one way to get good at anything you surround yourself with the best who are doing exactly what you do and you force yourself to keep up and inspire each other.',
+    },
+    {
+        id: 3,
+        name: 'Jay Z',
+        job: 'Artist',
+        image: 'https://www.billboard.com/wp-content/uploads/2022/09/jay-z-40-40-2021-billboard-1548.jpg?w=942&h=623&crop=1',
+        text: 'Belief in oneself and knowing who you are, I mean, that is the foundation for everything great.',
+    },
+    {
+        id: 4,
+        name: 'Jordan Peterson',
+        job: 'Clinical Psychologist',
+        image: 'https://exxpress.at/media/2023/01/jordan-peterson-shutterstock.jpg',
+        text: "Don't compare yourself with other people; compare yourself with who you were yesterday.",
+    },
+];
+
+export default reviews;
