e84b834b562fdbf23d4ac0195ff4021ae4e846cd
Oerd Bej <oerdbej@gmail.com>
2023-04-25 09:28:15 +0200

    react icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import people from './components/data';
+import {
+    FaChevronCircleLeft,
+    FaChevronCircleRight,
+    FaQuoteRight,
+} from 'react-icons/fa';
 
 const App = () => {
     const [index, setIndex] = useState(0);
@@ -7,7 +12,21 @@ const App = () => {
     const { name, job, image, text } = people[index];
     console.log(name);
 
-    return <div>App</div>;
+    return (
+        <main>
+            <article className='review'>
+                <div className='img-container'>
+                    <img src={image} alt={name} />
+                    <span>
+                        <FaQuoteRight />
+                    </span>
+                </div>
+                <h4>{name}</h4>
+                <p>{job}</p>
+                <p>{text}</p>
+            </article>
+        </main>
+    );
 };
 
 export default App;
